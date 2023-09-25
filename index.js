4910715d0b083f721bba38f9c629bc228397c872
Count-MonteCristo <79714613+Count-MonteCristo@users.noreply.github.com>
2023-09-25 10:45:40 -0500

    adds related data and  mutations

--- index.js
+++ index.js
@@ -28,6 +28,51 @@ const resolvers = {
       return _db.authors.find((author) => author.id === args.id);
     },
   },
+  Game: {
+    reviews(parent) {
+      return _db.reviews.filter((r) => r.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return _db.reviews.filter((r) => r.author_id === parent.id);
+    },
+  },
+  Review: {
+    author(parent) {
+      return _db.authors.find((a) => a.id === parent.author_id);
+    },
+    game(parent) {
+      return _db.games.find((g) => g.id === parent.game_id);
+    },
+  },
+  Mutation: {
+    deleteGame(_, args) {
+      _db.games = _db.games.filter((g) => g.id !== args.id);
+      return _db.games;
+    },
+    addGame(_, args) {
+      let game = {
+        ...args.game,
+        id: Math.floor(Math.random() * 10000).toString(),
+      };
+
+      _db.games.push(game);
+
+      return game;
+    },
+    updateGame(_, args) {
+      _db.games = _db.games.map((g) => {
+        if (g.id === args.id) {
+          return { ...g, ...args.edits };
+        }
+
+        return g;
+      });
+
+      return _db.games.find((g) => g.id === args.id);
+    },
+  },
 };
 
 // server setup
